82ccc8a09d5425cadcd226598d4a1bc18e2274fb
Ming <mail@zyming.me>
2026-04-08 18:06:08 +0200

    Add capabilities to run end2end integration tests

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use axum::{
 mod users;
 
 async fn health_check_handler() -> impl IntoResponse {
-    const MESSAGE: &str = "Simple rest API response";
+    const MESSAGE: &str = "alive and ready";
 
     let json_response = serde_json::json!({
         "status": "connected",
@@ -18,14 +18,20 @@ async fn health_check_handler() -> impl IntoResponse {
     Json(json_response)
 }
 
+fn create_app() -> Router {
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/api/v1.0/healthcheck", get(health_check_handler))
+}
+
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .route("/api/healthcheck", get(health_check_handler));
-
+    let app = create_app();
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
+#[cfg(feature = "integration")]
+mod integration_tests;
