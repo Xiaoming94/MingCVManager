03640c514ba1f62149e92c8da7a885b5f578f2e0
Xiaoming <mail@zyming.me>
2026-07-02 15:42:44 +0200

    Create alias for googletest::result to avoid name collision

--- src/users/tests.rs
+++ src/users/tests.rs
@@ -1,10 +1,12 @@
 use googletest::prelude::*;
-use googletest::{Result, verify_that};
+use std::result::Result;
 
 use super::*;
 
+type TestResult<T> = googletest::Result<T>;
+
 #[test]
-fn users_are_created_with_username_and_id() -> Result<()> {
+fn users_are_created_with_username_and_id() -> TestResult<()> {
     let uuid = Uuid::from_u128(0x1337);
     let username = "username";
     let user = User::new(uuid, username);
